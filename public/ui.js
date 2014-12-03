ae7f48e529c7ddb549fb92483294ad2ddd04874d
Özhan Kaya <Fizzzo42@gmail.com>
2014-12-03 13:11:59 +0100

    Moved ui.js back to Client.html

--- public/ui.js
+++ public/ui.js
@@ -2,57 +2,59 @@
 
 (function ($) {
 
-    var socket = io.connect('http://localhost:4730/');
+    $( document ).ready(function (){
 
-    var redditApp = angular.module('RedditApp');
+        var socket = io.connect('http://localhost:4730/');
 
-    socket.on('connect', function (data) {
-        console.log("HAHA");
-        ohSnap('We are connected', 'green');
-        mydata.getEntries(function (data) {
-            refreshEntries(data);
+        var redditApp = angular.module('RedditApp');
+
+        socket.on('connect', function (data) {
+            console.log("HAHA");
+            ohSnap('We are connected', 'green');
+            mydata.getEntries(function (data) {
+                refreshEntries(data);
+            });
+            //socket.emit('addPost');
         });
-        //socket.emit('addPost');
-    });
-    socket.on('disconnect', function (data) {
-        console.log("BYE");
-        ohSnap('Connection lost!', 'red');
-    });
-
-    socket.on('message', function (data) {
-        switch(data.action){
-            case 'AddLink':
-            ohSnap('New Link loaded!', 'blue');
-            break;
-        }
-
-        console.log("Message received: " + data.action);
-
-        mydata.getEntries(function (data) {
-            refreshEntries(data);
+        socket.on('disconnect', function (data) {
+            console.log("BYE");
+            ohSnap('Connection lost!', 'red');
         });
 
-    });
+        socket.on('message', function (data) {
+            switch(data.action){
+                case 'AddLink':
+                ohSnap('New Link loaded!', 'blue');
+                break;
+            }
 
-    var refreshEntries = function(data){
-        var $scope = angular.element($('#content')).scope();
-        //var $scope = angular.element('$0').scope();
-        $scope.$apply(function() {
-            $scope.entries = data;
+            console.log("Message received: " + data.action);
+
+            mydata.getEntries(function (data) {
+                refreshEntries(data);
+            });
+
         });
-    };
 
-    //if(sessionStorage.redditName && sessionStorage.redditPassword)
-        //alert("todo, login");
+        var refreshEntries = function(data){
+            var $scope = angular.element($('#content')).scope();
+            //var $scope = angular.element('$0').scope();
+            $scope.$apply(function() {
+                $scope.entries = data;
+            });
+        };
 
-    //function rateUp()
-    //window.data.postEntry("Neu","www.blaaa.com");
-    
-    /*socket.on('message', function (data) {
-        alert("We got a message");
-    })*/
+        //if(sessionStorage.redditName && sessionStorage.redditPassword)
+            //alert("todo, login");
 
-    
+        //function rateUp()
+        //window.data.postEntry("Neu","www.blaaa.com");
+        
+        /*socket.on('message', function (data) {
+            alert("We got a message");
+        })*/
+
+    });
 
 
 })(jQuery);
